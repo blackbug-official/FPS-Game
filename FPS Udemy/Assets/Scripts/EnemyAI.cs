fdf751b52a0e183563ed9fa74df8b2cd22f70a53
blackbug-official <68537298+blackbug-official@users.noreply.github.com>
2020-07-21 17:19:38 +0530

    added , fpcamera , enmy with ai

---
+++ FPS Udemy/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
+    float distanceFromTarget = Mathf.Infinity;
+    NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceFromTarget = Vector3.Distance(target.position, transform.position);
+        if(distanceFromTarget <= chaseRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
